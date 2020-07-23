7df8a35d69342a01a65c11d0e0dcc06fdf385922
Param Thakkar <pht0104@gmail.com>
2020-07-23 13:07:36 -0400

    Clean up imports

--- src/main/java/me/param/plugins/deathmatch/Deathmatch.java
+++ src/main/java/me/param/plugins/deathmatch/Deathmatch.java
@@ -1,14 +1,10 @@
 package me.param.plugins.deathmatch;
 
-import me.param.plugins.deathmatch.events.OnBreakBlock;
-import me.param.plugins.deathmatch.events.OnDeath;
-import me.param.plugins.deathmatch.events.OnPlayerHit;
-import me.param.plugins.deathmatch.events.OnPlayerJoin;
+import me.param.plugins.deathmatch.events.*;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.*;
 import org.bukkit.scoreboard.*;
 
 public final class Deathmatch extends JavaPlugin {
